d43b554289801e98a8b479f4d4a5e1a8ff65c3bc
Gabe Klein <gabe@termtm.com>
2026-01-30 14:01:20 -0500

    refactor: simplify getName function by removing switch-case and improving readability

--- packages/babel-plugin-jsx/src/names.ts
+++ packages/babel-plugin-jsx/src/names.ts
@@ -3,12 +3,8 @@ import {
   AssignmentExpression,
   Class,
   Function,
-  FunctionDeclaration,
   JSXElement,
-  LabeledStatement,
-  ObjectProperty,
   VariableDeclaration,
-  VariableDeclarator,
 } from "@babel/types";
 
 import t from "./types";
@@ -41,98 +37,92 @@ export function getNames(path: NodePath<JSXElement>) {
 export function getName(path: NodePath): string {
   let encounteredReturn;
 
-  while (path)
-    switch (path.type) {
-      case "LabeledStatement":
-        return (<LabeledStatement>path.node).label.name;
-
-      case "VariableDeclarator": {
-        const { id } = path.node as VariableDeclarator;
-        return t.isIdentifier(id)
-          ? id.name
-          : (<VariableDeclaration>path.parentPath!.node).kind;
-      }
-
-      case "AssignmentExpression":
-      case "AssignmentPattern": {
-        const { left } = path.node as AssignmentExpression;
-        return t.isIdentifier(left) ? left.name : "assignment";
-      }
-
-      case "FunctionDeclaration":
-        return (<FunctionDeclaration>path.node).id!.name;
-
-      case "ExportDefaultDeclaration":
-        return getLocalFilename(path.hub);
-
-      case "ArrowFunctionExpression": {
-        path = path.parentPath!;
-        continue;
-      }
-
-      case "ReturnStatement": {
-        if (encounteredReturn) return "return";
-
-        encounteredReturn = path;
-
-        const ancestry = path.getAncestry();
-        const within = ancestry.find((x) =>
-          x.isFunction()
-        ) as NodePath<Function>;
-
-        const { node } = within;
-
-        if ("id" in node && node.id) return node.id.name;
-
-        if (t.isObjectMethod(node)) {
-          path = within.getAncestry()[2];
-          continue;
-        }
-
-        if (t.isClassMethod(node)) {
-          if (node.key.type !== "Identifier") return "ClassMethod";
-
-          if (node.key.name != "render") return node.key.name;
-
-          const owner = within.parentPath.parentPath as NodePath<Class>;
-
-          if (owner.node.id) return owner.node.id.name;
-
-          path = owner.parentPath;
-          continue;
-        }
-
-        path = within.parentPath;
-        continue;
-      }
-
-      case "ObjectProperty": {
-        const { key } = path.node as ObjectProperty;
-        return t.isIdentifier(key)
-          ? key.name
-          : t.isStringLiteral(key)
-          ? key.value
-          : "property";
-      }
-
-      default:
-        return "element";
+  while (path){
+    if(path.isLabeledStatement()) {
+      return path.node.label.name;
     }
 
-  return "element";
-}
+    if(path.isVariableDeclarator()) {
+      const { id } = path.node;
+      return t.isIdentifier(id) ? id.name : (path.parent as VariableDeclaration).kind;
+    }
 
-function getLocalFilename(hub: Hub) {
-  try {
-    const { basename, dirname, sep: separator } = require("path");
+    if(path.isAssignmentExpression() || path.isAssignmentPattern()) {
+      const { left } = path.node as AssignmentExpression;
+      return t.isIdentifier(left) ? left.name : "assignment";
+    }
 
-    const url = (hub as any).file.opts.filename as string;
-    const [base] = basename(url).split(".");
+    if(path.isFunctionDeclaration()) {
+      return path.node.id!.name;
+    }
 
-    if (base !== "index") return base;
+    if(path.isExportDefaultDeclaration()) {
+      try {
+        const { basename, dirname, sep: separator } = require("path");
 
-    return dirname(url).split(separator).pop()!;
-  } catch (err) {
-    return "File";
+        const url = (path.hub as any).file.opts.filename as string;
+        const [base] = basename(url).split(".");
+
+        if (base !== "index") return base;
+
+        return dirname(url).split(separator).pop()!;
+      } catch (err) {
+        return "File";
+      }
+    }
+
+    if(path.isArrowFunctionExpression()) {
+      path = path.parentPath!;
+      continue;
+    }
+
+    if(path.isReturnStatement()) {
+      if (encounteredReturn) return "return";
+
+      encounteredReturn = path;
+
+      const ancestry = path.getAncestry();
+      const within = ancestry.find((x) =>
+        x.isFunction()
+      ) as NodePath<Function>;
+
+      const { node } = within;
+
+      if ("id" in node && node.id) return node.id.name;
+
+      if (t.isObjectMethod(node)) {
+        path = within.getAncestry()[2];
+        continue;
+      }
+
+      if (t.isClassMethod(node)) {
+        if (node.key.type !== "Identifier") return "ClassMethod";
+
+        if (node.key.name != "render") return node.key.name;
+
+        const owner = within.parentPath.parentPath as NodePath<Class>;
+
+        if (owner.node.id) return owner.node.id.name;
+
+        path = owner.parentPath;
+        continue;
+      }
+
+      path = within.parentPath;
+      continue;
+    }
+
+    if(path.isObjectProperty()) {
+      const { key } = path.node;
+      return t.isIdentifier(key)
+        ? key.name
+        : t.isStringLiteral(key)
+        ? key.value
+        : "property";
+    }
+
+    return "element";
   }
+
+  return "element";
 }
