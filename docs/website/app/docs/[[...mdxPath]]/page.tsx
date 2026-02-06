3a8959199c64db8d6e4b4c883fa45132e1454e15
Gabe Klein <gabe@termtm.com>
2026-02-05 21:11:40 -0500

    feat: implement DocsLayout and update layout structure for improved documentation presentation

--- docs/website/app/docs/[[...mdxPath]]/page.tsx
+++ docs/website/app/docs/[[...mdxPath]]/page.tsx
@@ -1,24 +1,27 @@
-import { generateStaticParamsFor, importPage } from 'nextra/pages';
-// import { useMDXComponents } from '@/mdx-components';
+import { generateStaticParamsFor, importPage } from 'nextra/pages'
+import { useMDXComponents } from '@/mdx-components';
 
-export const generateStaticParams = generateStaticParamsFor('mdxPath');
+export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
 export async function generateMetadata(props) {
-  const params = await props.params;
-  const { metadata } = await importPage(params.mdxPath);
-  return metadata;
+  const params = await props.params
+  const { metadata } = await importPage(params.mdxPath)
+  return metadata
 }
 
-// const Wrapper = useMDXComponents().wrapper;
+const Wrapper = useMDXComponents().wrapper
 
 export default async function Page(props) {
-  const params = await props.params;
-  const result = await importPage(params.mdxPath);
-  const { default: MDXContent, toc, metadata } = result;
-
+  const params = await props.params
+  const {
+    default: MDXContent,
+    toc,
+    metadata,
+    sourceCode
+  } = await importPage(params.mdxPath)
   return (
-    // <Wrapper toc={toc} metadata={metadata}>
+    <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
       <MDXContent {...props} params={params} />
-    // </Wrapper>
-  );
+    </Wrapper>
+  )
 }
--- docs/website/app/layout.tsx
+++ docs/website/app/layout.tsx
@@ -1,7 +1,4 @@
 import type { Metadata } from "next";
-import { Layout, Navbar, Footer } from "nextra-theme-docs";
-import { getPageMap } from "nextra/page-map";
-import { SiteLogo } from "@/components/Logo";
 import "nextra-theme-docs/style.css";
 
 export const metadata: Metadata = {
@@ -9,33 +6,7 @@ export const metadata: Metadata = {
   description: "Build-time CSS-in-JS using upcycled JavaScript syntax",
 };
 
-const Root: React.FC<React.PropsWithChildren> = async (props) => {
-  const pageMap = await getPageMap();
-
-  return (
-    <Html>
-      <Layout
-        pageMap={pageMap}
-        sidebar={{ defaultMenuCollapseLevel: 2 }}
-        navbar={
-          <Navbar
-            logo={<SiteLogo />}
-            projectLink="https://github.com/gabeklein/expressive-jsx"
-          />
-        }
-        footer={
-          <Footer>
-            <p>MIT 2024 © Gabe Klein</p>
-          </Footer>
-        }
-      >
-        {props.children}
-      </Layout>
-    </Html>
-  );
-};
-
-const Html: React.FC<React.PropsWithChildren> = ({ children }) => (
+const Root: React.FC<React.PropsWithChildren> = ({ children }) => (
   <html lang="en" suppressHydrationWarning>
     <head>
       <link href="https://fonts.googleapis.com" rel="preconnect" />
