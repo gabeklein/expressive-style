c7159aa39a0b85e80a43dd55b7e5be69346d5bd1
Gabe Klein <gabe@termtm.com>
2026-02-06 11:54:32 -0500

    feature: initialize new docs

---
+++ docs/fumadocs/waku.config.ts
@@ -0,0 +1,16 @@
+import stylePlugin from "@expressive/vite-plugin";
+import { type Config, defineConfig } from "waku/config";
+import mdx from "fumadocs-mdx/vite";
+import * as MdxConfig from "./source.config.js";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import type { UserConfig } from "vite";
+
+export default defineConfig({
+  vite: {
+    optimizeDeps: {
+      exclude: ["fumadocs-ui", "fumadocs-core", "@fumadocs/ui"],
+    },
+    plugins: [tailwindcss(), mdx(MdxConfig), tsconfigPaths(), stylePlugin()],
+  } satisfies UserConfig as Config["vite"],
+});
