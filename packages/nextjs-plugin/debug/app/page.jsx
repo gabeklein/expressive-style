75f1a8045832bcb649739c557fb55b4f5a393024
Gabe Klein <gabe@termtm.com>
2026-02-23 23:49:50 -0500

    feat: add Timer component to display uptime in Home page

--- packages/nextjs-plugin/debug/app/page.jsx
+++ packages/nextjs-plugin/debug/app/page.jsx
@@ -1,12 +1,26 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => setSeconds(s => s + 1), 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  return <span>Uptime: {seconds}s</span>;
+}
+
 export default function Home() {
-  color: red;
+  color: green;
   fontSize: 24;
 
   return (
     <div>
       <div>Client Component - Expressive JSX is working!</div>
+      <Timer />
       <nav>
         <a href="/ssr">SSR Page</a>
         {" | "}
